19152010cc333e0bd5da92d12bc76f2be3428c19
Irfan Syahmi Mohd Isa <s10189620@connect.np.edu.sg>
2020-11-10 21:07:13 +0800

    New line of print text added

--- Hello_world.py
+++ Hello_world.py
@@ -1,1 +1,8 @@
 print('hello world')
+
+# New line of print text 1 added
+print('This is hello from Irfan. I am adding a new line of code here.')
+
+print('Hello again. I am here to  add in a second line. I am Irfan.
+')
+
