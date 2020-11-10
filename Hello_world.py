aa32953e3363d81c73ca95c6ac5044559cc45379
Irfan Syahmi Mohd Isa <s10189620@connect.np.edu.sg>
2020-11-10 21:09:55 +0800

    Fixed EOL of line error

--- Hello_world.py
+++ Hello_world.py
@@ -2,7 +2,4 @@ print('hello world')
 
 # New line of print text 1 added
 print('This is hello from Irfan. I am adding a new line of code here.')
-
-print('Hello again. I am here to  add in a second line. I am Irfan.
-')
-
+print('Hello again. I am here to  add in a second line. I am Irfan.')
