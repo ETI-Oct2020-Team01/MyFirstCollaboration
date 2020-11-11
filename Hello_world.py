766e2a5be3ac1cdddffc300fa50a2345fe71d371
nurinnazurah <S10189240@connect.np.edu.sg>
2020-11-11 15:07:59 +0800

    New line of print text 2 added

--- Hello_world.py
+++ Hello_world.py
@@ -2,3 +2,4 @@ print('hello world')
 
 ##New line of print text 1 added
 print('This is hello from Nazurah. I am adding a new line of code.')
+print('Hello again, I am to add in a second line. I am Nazurah')
